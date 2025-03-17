9a1b5ccdbd0fd5cd789b7db47590dfa3ca9fef06
Lucas Oliveira <lqsoliveira@gmail.com>
2025-03-17 14:47:12 -0300

    Inclusão das funções: limparCampo, adicionarAmigo, e correção do erro favicon.ico no html com a inclusão de um favicon.ico.

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = [];
+
+function limparCampo(){
+    amigoNome = document.getElementById('amigo');
+    amigoNome.value = '';
+};
+
+function adicionarAmigo(){
+    let amigoNome = document.getElementById('amigo').value;
+    if (amigoNome==''){
+        alert("Por favor, insira um nome.")
+    }else{
+        amigos.push(amigoNome);
+        limparCampo();
+    }
+};
